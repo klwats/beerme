d53d262b74c2412187d9ef988af6b00316e9e807
Kelli Watkins <watkinskellil@gmail.com>
2023-06-06 21:06:18 -0600

    Add states to dropdown menu

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import "./DropDown.css";
-import { fetchStates } from '../API/apiCalls';
-
-const Icon = () => {
-    return (
-        <svg height="20" width="20" viewBox="0 0 20 20">
-            <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-        </svg>
-    )
-}
+import './Dropdown.css';
 
 
-const Dropdown = ({ placeholder, options }) => {
-    const getDisplay = () => {
-        return placeholder;
+
+
+const Dropdown = ({ selectOption, getOptions }) => {
+
+    const handleChange = (e) => {
+        selectOption(e.target.value)
     }
 
-    function getStates() {
-        fetchStates().then(data => {
-            return (data).map(state => {
-                return (
-                    <option key={state.id} value={state.id}>{state.name}</option>
-                )
-            })
-                .catch(error => {
-                    throw new Error(error);
-                })
-        })
 
-        return (
-
-            <div className="dropdown-container">
-                <div className="dropdown-input">
-                    <div className="dropdown-selected-value">{getDisplay()}</div>
-                    <div className="dropdown-menu">{getStates()}</div>
-
-                    <div className="dropdown-tools">
-                        <div className="dropdown-tool">
-                            <Icon />
-                        </div>
-                    </div>
-                </div>
+    return (
+        <main className='dropdown-container'>
+            <div>
+                <h1>Select a State</h1>
+                <form>
+                    <select className="dropdown-menu" name="states" id="states" onChange={handleChange}>
+                        <option value='' defaultValue>Select Your State</option>
+                        {getOptions().map((option, index) => (
+                            <option key={index} value={option}>{option}</option>
+                        ))}
+                    </select>
+                </form>
             </div>
-        );
-    };
-}
+        </main>
+
+
+
+        // <div className="dropdown-container">
+        //     <div className="dropdown-input">
+        //         {/* <div className="dropdown-selected-value">{getDisplay()}</div> */}
+        //         <div className="dropdown-menu">{options}</div>
+
+
+        //         <div className="dropdown-tools">
+        //             <div className="dropdown-tool">
+        //                 <Icon />
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+    );
+};
+
 
 export default Dropdown;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>BeerMe</h1>
+        </div>
+    )
+}
+
+export default Header
+
